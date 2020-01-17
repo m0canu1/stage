fa2bd229e2a2d881c81a8aa4988102a4678b0cbb
m0canu1 <alexandrumocanu9@gmail.com>
2020-01-17 14:33:53 +0100

    ss

--- python/phaseconfig.py
+++ python/phaseconfig.py
@@ -35,6 +35,7 @@ def phase_one(nteams):
 
 def phase_two(config2, nteams):
     
+    # TODO problema col for
     for i in range(0,nteams):
         address = input("""Team %d address: """ %(i))
         config2['network']['ethernets']['ens%d'%(first_team_interface+i)]['addresses'] = [address + '/24']
