f71b4c1a100b1ce53b7bbba514b545c525e15bd2
m0canu1 <alexandrumocanu9@gmail.com>
2020-01-20 11:39:22 +0100

    fff

--- python/cfg.py
+++ python/cfg.py
@@ -1,32 +1,36 @@
 import yaml
 import sys
-from phaseconfig import phase_one, phase_two, remove_quotes
+import json
+from phaseconfig import phase_one, phase_two, edit_vr_address, edit_mma_address, set_teams_number
 
-f1 = "/home/alex/Desktop/git/stage/yaml/phaseone.yaml"
-f2 = "/home/alex/Desktop/git/stage/yaml/phasetwo.yaml"
-
-
-config = {'network': 'ethernets'}
 
 menu = -1
-nteams = 0
-nteams = int(input("""Number of teams: """))
-print("Choose Competition phase")
+print("Make a choice:")
 while menu == -1:
     menu = int(input("""
         1. Phase 1 (only management allowed).
         2. Phase 2 (All teams allowed).
+        3. Set Virtual Router Address
+        4. Set Management Machine Address
+        5. Set teams number
         0. Quit.
         """))
     if (menu) == 1:
-        out_file = open(f1, 'w')
-        yaml.dump(phase_one(nteams), out_file)
-        print("Created configuration for Pha6se 1")
+        phase_one()
+        print("Created configuration for Phase 1")
     elif (menu) == 2:
-        config = yaml.load(open(f1, 'r'), Loader=yaml.FullLoader)
-        yaml.dump(phase_two(config, nteams), open(f2, 'w'))
-        remove_quotes(f2)
+        phase_two()
         print("Created configuration for Phase 2")
+    elif (menu) == 3:
+        print("Not yet available.")
+        # edit_config("VirtualRouterAddress")
+    elif (menu) == 4:
+        print("Not yet available.")
+        # edit_config("ManagementMachineAddress")
+    elif (menu) == 5:
+        print("Not yet available.")
+        # edit_config("NumberOfTeams")
+
     elif (menu) == 0:
         sys.exit
     else:
--- python/phaseconfig.py
+++ python/phaseconfig.py
@@ -1,13 +1,17 @@
 import yaml
+import json
 
-config = {'network': {'ethernets': {'ens33': {'dhcp4': False, 'dhcp6': False},
-                                    # ,
-                                    'ens38': {'dhcp4': False, 'dhcp6': False}
-                                    # 'ens39': {'dhcp4': False, 'dhcp6': False},
-                                    # 'ens40': {'dhcp4': False, 'dhcp6': False},
-                                    # 'ens41': {'dhcp4': False, 'dhcp6': False}
-                                    },
-                      'version': 2}}
+configfile = "/home/alex/Desktop/git/stage/config/competition.config"
+f1 = "/home/alex/Desktop/git/stage/yaml/phaseone.yaml"
+f2 = "/home/alex/Desktop/git/stage/yaml/phasetwo.yaml"
+
+default_config = {'network': {'ethernets': {'ens33': {'dhcp4': False, 'dhcp6': False},
+                                            'ens38': {'dhcp4': False, 'dhcp6': False},
+                                            'version': 2}}}
+
+competition_config = {'VirtualRouterAddress': 0,
+                      'ManagementMachineAddress': 0,
+                      'NumberOfTeams': 0}
 
 default_gateway = '0.0.0.0'
 
@@ -22,36 +26,62 @@ def address_to(address):
     return address + '0'
 
 
-def phase_one(nteams):
+def phase_one():
+    nteams = int(input("""Number of teams: """))
+    competition_config['NumberOfTeams'] = nteams
+
     address = str(input("""Virtual Router address: """))
-    config['network']['ethernets']['ens33']['addresses'] = [address + '/24']
+    default_config['network']['ethernets']['ens33']['addresses'] = [
+        address + '/24']
+    competition_config['VirtualRouterAddress'] = address
+
     address = str(input("""Management Machine address: """))
-    config['network']['ethernets']['ens38']['addresses'] = [address + '/24']
+    default_config['network']['ethernets']['ens38']['addresses'] = [
+        address + '/24']
+    competition_config['ManagementMachineAddress'] = address
+
+    with open(configfile, 'w') as f:
+        json.dump(competition_config, f)
 
     for i in range(0, nteams):
         dhcp = {'dhcp4': False, 'dhcp6': False}
-        config['network']['ethernets']['ens' +
-                                       str(first_team_interface+i)] = [dhcp]
-    return config
+        default_config['network']['ethernets']['ens' +
+                                               str(first_team_interface+i)] = [dhcp]
+    
+    with open(f1,'w') as phase_one:
+        yaml.safe_dump(default_config, phase_one)
 
+def phase_two():
+    with open(configfile) as f:
+        competition_config = json.load(f)
+    nteams = competition_config["NumberOfTeams"]
+    vr_address = competition_config["VirtualRouterAddress"] + '/24'
+    mma_address = competition_config["ManagementMachineAddress"] + '/24'
 
-def phase_two(config2, nteams):
+    config = yaml.load(open(f1, 'r'), Loader=yaml.FullLoader)
+
+    config['network']['ethernets']['ens33']['addresses'] = [vr_address]
+    config['network']['ethernets']['ens38']['addresses'] = [mma_address]
+
 
     for i in range(0, nteams):
         team_address = input("""Team %d address: """ % (i+1))
         addresses = {'addresses': [str(team_address + '/24')]}
         routes = [
             {'to': address_to(team_address) + '/24', 'via': default_gateway}]
-        config2['network']['ethernets']['ens' +
-                                        str(first_team_interface+i)] = addresses
-        config2['network']['ethernets']['ens' +
-                                        str(first_team_interface+i)]['routes'] = routes
-        config2['network']['ethernets']['ens' +
-                                        str(first_team_interface+i)]['dhcp4'] = False
-        config2['network']['ethernets']['ens' +
-                                        str(first_team_interface+i)]['dhcp6'] = False
+        config['network']['ethernets']['ens' +
+                                       str(first_team_interface+i)] = addresses
+        config['network']['ethernets']['ens' +
+                                       str(first_team_interface+i)]['routes'] = routes
+        config['network']['ethernets']['ens' +
+                                       str(first_team_interface+i)]['dhcp4'] = False
+        config['network']['ethernets']['ens' +
+                                       str(first_team_interface+i)]['dhcp6'] = False
+    
+    yaml.dump(config, open(f2, 'w'))
+    remove_quotes(f2)
 
-    return config2
+    # return config2
 
 
 def remove_quotes(fname):
@@ -60,3 +90,28 @@ def remove_quotes(fname):
     data = data.replace("'", "")
     file = open(fname, 'w')
     file.write(data)
+
+
+def edit_vr_address():
+    with open(configfile,'r') as f:
+        conf = json.load(f)
+    new_val = str(input("Address of Virtual Router: "))
+    conf["VirtualRouterAddress"] = new_val
+    with open(configfile,'w') as f:
+        json.dump(conf, f)
+
+def edit_mma_address():
+    with open(configfile, 'r') as f:
+        conf = json.load(f)
+    new_val = str(input("Address of Management machine: "))
+    conf["ManagementMachineAddress"] = new_val
+    with open(configfile, 'w') as f:
+        json.dump(conf, f)
+
+def set_teams_number():
+    with open(configfile, 'r') as f:
+        conf = json.load(f)
+    new_val = str(input("Number of teams: "))
+    conf["NumberOfTeams"] = new_val
+    with open(configfile, 'w') as f:
+        json.dump(conf, f)
