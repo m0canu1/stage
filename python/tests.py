6396c86fd15fecfd2dcf5d7514a919241a374fd5
Alexandru Mocanu <alexandrumocanu9@gmail.com>
2020-02-12 14:07:31 +0100

    aggiunto timestamp del test e corretto bug output dell'errore

--- python/tests.py
+++ python/tests.py
@@ -2,6 +2,7 @@ import subprocess
 import json
 import socket
 import argparse
+import datetime
 
 
 def ping_test(ip):
@@ -29,7 +30,7 @@ def iperf_udp_test(ip):
             ['iperf', '-c', ip, '-u'],
             # timeout=10, # NON SERVE TIMEOUT PER LA TIPOLOGIA DI RISPOSTA DI UDP
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
             universal_newlines=True  # return string not bytes
         )
 
@@ -49,7 +50,7 @@ def iperf_tcp_test(ip):
             ['iperf', '-c', ip],
             timeout=15,
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
             universal_newlines=True  # return string not bytes
         )
 
@@ -77,16 +78,16 @@ def run_test(str, flag):
 
 
 ip_list = [
-            "172.168.1.128",
-            "172.168.2.100",
-            "172.168.2.128",
-            "172.168.3.100",
-            "172.168.3.130",
-            "172.168.4.100",
-            "172.168.4.128",
-            "172.168.5.100",
-            "172.168.5.128"
-          ]
+    "172.168.1.128",
+    "172.168.2.100",
+    "172.168.2.128",
+    "172.168.3.100",
+    "172.168.3.130",
+    "172.168.4.100",
+    "172.168.4.128",
+    "172.168.5.100",
+    "172.168.5.128"
+]
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--verbose',
@@ -95,9 +96,10 @@ parser.add_argument('-t', '--test',
                     help='which test to perform (different combinations between tcp, udp, ping)', nargs='+')
 args = parser.parse_args()
 
-print (args.verbose)
-print (args.test)
+# print (args.verbose)
+# print (args.test)
 
+print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 print("\nTEST FROM " + socket.gethostname() + ":\n\n")
 
 
